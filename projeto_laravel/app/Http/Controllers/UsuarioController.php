907438c8352abcba6fe7fe4d33c82a9520a59a6d
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-07 14:19:21 -0300

    Exercicio 100 projeto Laravel

--- projeto_laravel/app/Http/Controllers/UsuarioController.php
+++ projeto_laravel/app/Http/Controllers/UsuarioController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Usuario;
+use App\Models\Documento;
 
 class UsuarioController extends Controller
 {
     function index(){
         $usuarios = Usuario::index();
+        $documentos = Documento::all();
 
-        return view('usuario.perfil', compact('usuarios'));
+        return view('usuario.perfil', compact('usuarios', 'documentos'));
     }
 }
--- projeto_laravel/resources/views/usuario/perfil.blade.php
+++ projeto_laravel/resources/views/usuario/perfil.blade.php
@@ -1,2 +1,40 @@
 <h1>Olá {{ $usuarios['nome'] }} </h1>
 <h2>Sua idade é {{ $usuarios['idade'] }} anos!</h2>
+<br>
+<h2>Estes são os seus documentos:</h2>
+<table>
+    <tr>
+        <th>Título</th>
+        <th>Tamanho(Kbs)</th>
+        <th>Número de Assinaturas</th>
+        <th>Assinatura Responsável</th>
+        <th>Páginas</th>
+      </tr>
+@foreach($documentos as $documento)
+    <tr>
+      <th>{{ $documento['titulo'] }}</th>
+      <th>{{ $documento['tamanho'] }}kb</th>
+      <th>{{ $documento['num_assinaturas'] }}</th>
+      <th>{{ $documento['assinatura_responsavel'] }}</th>
+      <th>{{ $documento['qtd_paginas'] }}</th>
+    </tr>
+@endforeach
+</table>
+
+<style>
+    table {
+      font-family: arial, sans-serif;
+      border-collapse: collapse;
+      width: 100%;
+    }
+    
+    td, th {
+      border: 1px solid #dddddd;
+      text-align: left;
+      padding: 8px;
+    }
+    
+    tr:nth-child(even) {
+      background-color: #dddddd;
+    }
+    </style>
