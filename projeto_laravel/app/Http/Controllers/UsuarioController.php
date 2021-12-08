d89d53bb8708f9403ef1ff13bf08e1a3d077b2c2
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-08 11:30:22 -0300

    Exercicio 101 projeto Laravel

--- projeto_laravel/app/Http/Controllers/UsuarioController.php
+++ projeto_laravel/app/Http/Controllers/UsuarioController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Usuario;
 use App\Models\Documento;
+use Illuminate\Support\Facades\Log;
 
 class UsuarioController extends Controller
 {
     function index(){
         $usuarios = Usuario::index();
         $documentos = Documento::all();
+        Log::channel('documentos')->info('Usuários e Documentos consultados com sucesso!');
 
         return view('usuario.perfil', compact('usuarios', 'documentos'));
     }
