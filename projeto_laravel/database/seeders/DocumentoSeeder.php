6189756797708fc5c43cc5371ca754ca69d9a1e0
Evaldo R. C <evaldorubel@hotmail.com>
2021-12-20 17:26:13 -0300

    Exercicio 110

---
+++ projeto_laravel/database/seeders/DocumentoSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DocumentoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Documento::create([
+            'titulo' => 'Documento 1',
+            'tamanho' => '100',
+            'num_assinaturas' => '1',
+            'assinatura_responsavel' => '1',
+            'qtd_paginas' => '100'
+        ]);
+        \App\Models\Documento::create([
+            'titulo' => 'Documento 2',
+            'tamanho' => '200',
+            'num_assinaturas' => '2',
+            'assinatura_responsavel' => '2',
+            'qtd_paginas' => '200'
+        ]);
+        \App\Models\Documento::create([
+            'titulo' => 'Documento 3',
+            'tamanho' => '300',
+            'num_assinaturas' => '3',
+            'assinatura_responsavel' => '3',
+            'qtd_paginas' => '300'
+        ]);
+    }
+}
